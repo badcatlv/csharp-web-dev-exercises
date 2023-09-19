2bb57cc9f9fab95b4d141143f634e17595135e7d
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-19 14:48:34 -0500

    Exercise 8

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,19 @@
+﻿namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            double diff = x.Cost - y.Cost;
+            if (diff > 0)
+            {
+                return 1;
+            }
+            else if (diff < 0)
+            {
+                return -1;
+            }
+            else { return 0; }
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -6,8 +6,20 @@ List<Cone> availableCones = menu.Cones;
 
 // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
 //  field.
+FlavorComparer comparer = new FlavorComparer();
+availableFlavors.Sort(comparer);
+//availableFlavors.Sort(new FlavorComparer()); //short hand version
 
 // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
 //  field.
+availableCones.Sort(new ConeComparer());
+foreach (Flavor f in availableFlavors)
+{
+    Console.WriteLine(f.Name + ": " + f.Cost);
+}
+foreach (Cone c in availableCones)
+{
+    Console.WriteLine(c.Name + ": " + c.Cost);
+}
 
 // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
